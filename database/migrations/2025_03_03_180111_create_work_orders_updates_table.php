6821dda8defb0a531e5fe9d3059f214692457e35
afes.okta <afes.okta@gmail.com>
2025-03-04 01:07:08 +0700

    change project core

---
+++ database/migrations/2025_03_03_180111_create_work_orders_updates_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('work_orders_updates', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('work_order_id')->constrained('work_orders')->onDelete('cascade');
+            $table->enum('status', ['Pending', 'In Progress', 'Completed', 'Canceled']);
+            $table->integer('quantity_updated');
+            $table->text('notes')->nullable();
+            $table->foreignId('updated_by')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('work_orders_updates');
+    }
+};
